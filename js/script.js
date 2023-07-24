4ce73a0511b2c2dac36dcdab3b5f9833c1980647
IkebeKazuma <140383387+IkebeKazuma@users.noreply.github.com>
2023-07-24 17:14:15 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function() {
+	window.setTimeout(
+		function(){
+			$('body').fadeIn(500); // 0.5秒かけてフェードイン
+		},
+		100
+	);
+
+	$("#left-side-menu > a").click( function() {
+		var src = $(this).attr("href"); 
+		var main = $("#main-frame");
+		var mainFrame = $("#main-frame > iframe");
+
+		//フェードアウトして、iframeのソースかえて、フェードイン
+		main.fadeTo(200,0.05,function() {
+			mainFrame.attr("src", src);
+		});
+		return false;
+	});
+});
+
+function MainFrameFadeIn(){
+	$("#main-frame").fadeTo(200, 1);
+}
